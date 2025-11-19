51d6568711928b5986f857393e6e7903c668751d
Jonas Westberg <jowe@Jonass-MacBook-Air.local>
2025-11-19 21:59:51 +0100

    updated browser and vscode keys

--- data/browsers.js
+++ data/browsers.js
@@ -1,36 +1,38 @@
 const browsers = {
     application: "Browsers", sections: [
         {
-            name: "Modes", keys: [
-                { name: "Profile", safari: "ctrl cmd q", edge: "win l", brave: "" },
-            ]
-        },
-        {
             name: "Tabs", keys: [
                 { name: "New", safari: "cmd t", edge: "ctrl t", brave: "cmd t" },
                 { name: "Close", safari: "cmd w", edge: "ctrl w", brave: "cmd w" },
-                { name: "Next", safari: "shift cmd ]", edge: "?", brave: "" },
-                { name: "Prev", safari: "cmd left", edge: "?", brave: "" },
-                { name: "List", safari: "", edge: "?", brave: "shift cmd a" },
-                { name: "Reopen closed", safari: "", edge: "?", brave: "cmd shift t" }
+                { name: "Reopen closed", safari: "cmd shift t", edge: "ctrl shift t", brave: "cmd shift t" },
+                { name: "Right", safari: "ctrl tab", edge: "ctrl tab", brave: "ctrl tab" },
+                { name: "Left", safari: "ctrl shift tab", edge: "ctrl shift tab", brave: "ctrl shift tab" },
+                { name: "Previous used", safari: "", edge: "alt tab", brave: "cmd shift a" },
+                { name: "List/search", safari: "cmd shift '", edge: "", brave: "shift cmd a" },
             ]
         },
         {
             name: "Misc", keys: [
-                { name: "Passwords", safari: "", edge: "", brave: "cmd shift x" },
+                { name: "Passwords", safari: "cmd shift x", edge: "ctrl shift x", brave: "cmd shift x" },
                 { name: "Commands", safari: "", edge: "", brave: "cmd shift p" },
-                { name: "History", safari: "", edge: "", brave: "cmd y" },
-                { name: "Downloads", safari: "", edge: "", brave: "cmd shift j" },
-                { name: "Developer", safari: "cmd ,", edge: "", brave: "f12" }
+                { name: "History", safari: "cmd y", edge: "ctrl h", brave: "cmd y" },
+                { name: "Downloads", safari: "opt cmd l", edge: "ctrl j", brave: "cmd shift j" },
+                { name: "Developer", safari: "opt cmd i", edge: "", brave: "f12" }
             ]
         },
         {
             name: "Bookmarks", keys: [
-                { name: "edgedows", safari: "f3", edge: "?", brave: "" },
-                { name: "Next", safari: "cmd ´", edge: "?", brave: "" },
-                { name: "Prev", safari: "cmd shift ´", edge: "?", brave: "" },
-                { name: "Hide", safari: "cmd h", edge: "?", brave: "" },
+                { name: "Add", safari: "", edge: "?", brave: "" },
+                { name: "Search", safari: "cmd l", edge: "?", brave: "" },
             ]
-        }
+        },
+        {
+            name: "Find", keys: [
+                { name: "Find", safari: "cmd f", edge: "ctrl f", brave: "cmd f" },
+                { name: "Find next", safari: "cmd g", edge: "ctrl g", brave: "cmd g" },
+                { name: "Find previous", safari: "cmd shift g", edge: "ctrl shift g", brave: "cmd shift g" },
+                { name: "Find previous", safari: "cmd shift g", edge: "ctrl shift g", brave: "cmd shift g" },
+            ]
+        },
     ]
 };
--- data/editors.js
+++ data/editors.js
@@ -2,17 +2,18 @@
       application: "Coding ;-)", sections: [
         {
           name: "Navigation", keys: [
+            { name: "Explorer", vscode: "cmd shift e", vim: "?" },
+            { name: "Git", vscode: "ctrl shift g", vim: "?" },
             { name: "Commands", vscode: "cmd shift p", vim: "?" },
-            { name: "Editor", vscode: "", vim: "?" },
-            { name: "Console", vscode: "", vim: "?" },
-            { name: "Files", vscode: "cmd shift e", vim: "?" },
+            { name: "Editor", vscode: "cmd 1", vim: "?" },
+            { name: "Terminal", vscode: "", vim: "?" },
           ]
         },
         {
           name: "Go to", keys: [
             { name: "File", vscode: "cmd p", vim: "?" },
             { name: "Symbol in file", vscode: "cmd shift o", vim: "?" },
-            { name: "Symbole", vscode: "cmd t", vim: "?" },
+            { name: "Symbol", vscode: "cmd t", vim: "?" },
           ]
         },
         {
